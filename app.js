c8cfbfab784c9d628836d8a69c0d40afb878b013
cool-skr <rshobith04@gmail.com>
2025-04-05 00:22:30 +0530

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import connectDB from './DB/Connection.js';
 import router from './API/routes/User.js';
 
 const app = express();
 connectDB();
+app.use(cors()); 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(morgan('dev'));
